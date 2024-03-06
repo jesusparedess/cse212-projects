d700f777dd9d295de717ad4088405a07e71304f4
jesusparedess <jesusparedess@byui.edu>
2024-03-05 23:28:38 -0500

    test

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -18,6 +18,13 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new List<int>();
+        for (int i = 1; i < number; i++) {
+            // Use an if statement to check if 'i' is a divisor of 'number'
+            if (number % i == 0) {
+                // Call Add to add the divisor to the results list
+                results.Add(i);
+            }
+        }
         // Todo problem 1
         return results;
     }
