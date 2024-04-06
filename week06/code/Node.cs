f29af9c880a88659b4eebc36b94a73928761e79a
jesusparedess <jesusparedess@byui.edu>
2024-04-05 23:22:09 -0400

    problem1

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -15,7 +15,7 @@ public class Node {
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data) {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
